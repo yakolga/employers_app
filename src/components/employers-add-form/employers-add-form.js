a9572966799ddc35e49ada04e90746cfe66e1121
Olga Kulczycka <olga.kulczycka07@gmail.com>
2024-07-07 19:23:36 +0200

    dev: remake form function on component

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,23 +1,46 @@
+import {Component} from 'react';
 import './employers-add-form.css';
 
-const EmployersAddForm = () => {
-  return (
-    <div className="app-add-form">
-        <h3>Add a new employee</h3>
-        <form
-            className="add-form d-flex">
-            <input type="text"
-              className="form-control new-post-label"
-              placeholder="Name" />
-            <input type="number"
-              className="form-control new-post-label"
-              placeholder="Salary in $" />
+class EmployersAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: ''
+    };
+  }
 
-            <button type="submit"
-              className="btn btn-outline-light">Add</button>
-        </form>
-    </div>
-  )
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  render() {
+    const {name, salary} = this.state;
+    return (
+      <div className="app-add-form">
+          <h3>Add a new employee</h3>
+          <form
+              className="add-form d-flex">
+              <input type="text"
+                className="form-control new-post-label"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={this.onValueChange}/>
+              <input type="number"
+                className="form-control new-post-label"
+                placeholder="Salary in $"
+                name="salary"
+                value={salary}
+                onChange={this.onValueChange}/>
+              <button type="submit"
+                className="btn btn-outline-light">Add</button>
+          </form>
+      </div>
+    )
+  }
 }
 
 export default EmployersAddForm;
